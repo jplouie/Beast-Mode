55a7a122045dee7b6ca231cb6d43ef300f52a67d
Jeff Louie <jpl7690@yahoo.com>
2015-02-11 14:49:15 -0800

    add empty rails controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  resources :exercises
+  resources :exercise_sets
+  resources :time_of_days
+  resources :units
+  resources :users
+  resources :workouts
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
---
+++ test/helpers/time_of_days_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class TimeOfDaysHelperTest < ActionView::TestCase
+end
